699a7ae81b98a36b887aeaf9ac15cb49c61ca6ad
eliran <eliranmal@gmail.com>
2021-11-04 23:21:18 +0200

    refactor: reuse input component in crossfader

--- src/components/crossfader/Crossfader.js
+++ src/components/crossfader/Crossfader.js
@@ -1,9 +1,19 @@
+import Input from '../input/Input';
+
 import './Crossfader.css'
 
 
-const Crossfader = ({min = 0, max = 100, value = 50, ...props}) => (
-  <input className="Crossfader" type="range"
-    min={min} max={max} value={value}
+const Crossfader = ({
+  min = 0,
+  max = 100,
+  value = 50,
+  ...props}) => (
+  <Input
+    className={`Crossfader ${props.className}`}
+    type="range"
+    min={min}
+    max={max}
+    value={value}
     {...props} />
 )
 
