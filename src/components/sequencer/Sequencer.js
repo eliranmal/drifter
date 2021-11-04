a2059348b8c5026a5c78b5d2710566bd0109e8b4
eliran <eliranmal@gmail.com>
2021-11-05 00:14:51 +0200

    skeleton(sequencer): update look and feel

--- src/components/sequencer/Sequencer.js
+++ src/components/sequencer/Sequencer.js
@@ -1,24 +1,28 @@
+import {range} from '../../lib/util'
+import Input from '../input/Input'
+
 import './Sequencer.css'
 
 
-const Sequencer = ({className}) => (
+const Sequencer = ({
+  channels = 4,
+  // todo - create useMediaQueryListener and rely on screen width to determine the ticks
+  ticks = 32,
+  className}) => (
   <div className={`Sequencer ${className}`}>
-  {/*
-    <div className="Sequencer-gutter Sequencer-controls">
-      <div className="channel"></div>
-      <div className="channel"></div>
-      <div className="channel"></div>
-      <div className="channel"></div>
-    </div>
-    */}
     <div className="Sequencer-timeline">
       <div className="Sequencer-tape"></div>
       <div className="Sequencer-dots"></div>
-      <div className="Sequencer-channels">
-        <div className="channel"></div>
-        <div className="channel"></div>
-        <div className="channel"></div>
-        <div className="channel"></div>
+      <div
+        className="Sequencer-channels"
+        style={{ '--ticks-count': ticks }}>
+        {range(channels).map(channelIndex => (
+          <div key={channelIndex} className="Sequencer-channel">
+              {range(ticks).map(tickIndex => (
+                <Input type="checkbox" className="Sequencer-tick" key={tickIndex} />
+              ))}
+          </div>
+        ))}
       </div>
     </div>
   </div>
