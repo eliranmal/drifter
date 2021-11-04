67b4587f7f8ef11fd9ceaac7d7eb25c47c5af8ee
eliran <eliranmal@gmail.com>
2021-11-04 02:29:44 +0200

    skeleton: integrate transport into main view

--- src/pages/main-view/MainView.js
+++ src/pages/main-view/MainView.js
@@ -1,13 +1,15 @@
 
 import Sequencer from '../../components/sequencer/Sequencer'
+import Transport from '../../components/transport/Transport'
 
 import './MainView.css'
 
 
 const MainView = () => (
   <div className="MainView">
-    <h1 class="MainView-title">d&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;rifter</h1>
+    <h1 className="MainView-title">d&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;rifter</h1>
     <Sequencer className="MainView-panel" />
+    <Transport className="MainView-panel-center" />
   </div>
 )
 
