0d66c99c0e90d19aef4d8af9202d1272dd1f2a1c
eliran <eliranmal@gmail.com>
2021-11-04 02:43:31 +0200

    refactor: DRY, SR
    clean up neumorphism high-order component to be dumber

--- src/hoc/neumorphism/Neumorphism.js
+++ src/hoc/neumorphism/Neumorphism.js
@@ -2,5 +2,12 @@
 import './Neumorphism.css'
 
 
-export const withNeumorphism = (WrappedComponent, hasComplementColor) => props =>
-  <WrappedComponent {...props} className={`${props.className} Neumorphism`} />
+export const withNeumorphism = (WrappedComponent, { tintColor, shadeColor, backgroundColor } = {}) => props =>
+  <WrappedComponent
+    {...props}
+    className={`${props.className} Neumorphism`}
+    style={{
+      '--neumorphism-bg-color': backgroundColor,
+      '--neumorphism-shadow-color-tint': tintColor,
+      '--neumorphism-shadow-color-shade': shadeColor,
+    }} />
