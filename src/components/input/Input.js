0271499a9143bc18a678402e3e2f2c04fd9a9060
eliran <eliranmal@gmail.com>
2021-11-04 23:21:44 +0200

    fix: protect input from empty change handlers

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,10 +1,12 @@
 import './Input.css'
 
 
-const Input = ({ type = 'text', ...props}) => (
+const Input = ({ type = 'text', value, onChange = () => {}, ...props}) => (
   <input
     {...props}
     type={type}
+    value={value}
+    onChange={typeof value !== 'undefined' && value !== null ? onChange : null}
     className={`Input ${props.className ?? ''}`}
     />
 )
