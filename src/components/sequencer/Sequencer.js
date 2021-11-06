3ba8eff1b6ae0a678969c07c88bce4bb5f96609e
eliran <eliranmal@gmail.com>
2021-11-07 00:52:26 +0200

    feat: sequencer
    - bind sequencer state to transport
    - rethink sequencer tape
    - refactor: encapsulation

--- src/components/sequencer/Sequencer.js
+++ src/components/sequencer/Sequencer.js
@@ -1,31 +1,50 @@
+import {useState, useEffect} from 'react'
+
+import useMediaQueries from '../../hooks/useMediaQueries'
 import {range} from '../../lib/util'
 import Input from '../input/Input'
 
 import './Sequencer.css'
 
+// todo - externalize, persist
+const checkedTicks = [
+  [0, 6, 10, 16, 22, 26],
+  [4, 12, 20, 28],
+]
 
 const Sequencer = ({
-  channels = 4,
-  // todo - create useMediaQueryListener and rely on screen width to determine the ticks
-  ticks = 32,
-  className}) => (
-  <div className={`Sequencer ${className}`}>
-    <div className="Sequencer-timeline">
-      <div className="Sequencer-tape"></div>
-      <div
-        className="Sequencer-channels"
-        style={{ '--ticks-count': ticks }}>
-        {range(channels).map(channelIndex => (
-          <div key={channelIndex} className="Sequencer-channel">
-              {range(ticks).map(tickIndex => (
-                <Input type="checkbox" className="Sequencer-tick" key={tickIndex} />
-              ))}
-          </div>
-        ))}
-      </div>
-    </div>
-  </div>
-)
+  isRunning,
+  channelCount = 4,
+  initialTickCount = 20,
+  className}) => {
 
+    const [tickCount, setTickCount] = useState(initialTickCount)
+
+    useMediaQueries({
+      '(max-width: 600px)': setTickCount.bind(null, 4),
+      '(min-width: 600px) and (max-width: 800px)': setTickCount.bind(null, 8),
+      '(min-width: 800px) and (max-width: 1200px)': setTickCount.bind(null, 16),
+      '(min-width: 1200px)': setTickCount.bind(null, 32),
+    }, [setTickCount])
+
+    return (
+      <div
+        className={`Sequencer ${isRunning ? 'Sequencer-running' : ''} ${className}`}
+        style={{ '--sequencer-ticks': tickCount }}>
+        <div className="Sequencer-timeline">
+          <div className="Sequencer-tape"></div>
+          <div className="Sequencer-channels">
+            {range(channelCount).map(channelIndex => (
+              <div key={channelIndex} className="Sequencer-channel">
+                  {range(tickCount).map(tickIndex => (
+                    <Input type="checkbox" className="Sequencer-tick" key={tickIndex} defaultChecked={!!checkedTicks[channelIndex] && checkedTicks[channelIndex].includes(tickIndex)} />
+                  ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+}
 
 export default Sequencer
--- src/pages/main-view/MainView.js
+++ src/pages/main-view/MainView.js
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 
 import Sequencer from '../../components/sequencer/Sequencer'
 import Transport from '../../components/transport/Transport'
@@ -6,14 +7,27 @@ import Visualizations from '../../components/visualizations/Visualizations'
 import './MainView.css'
 
 
-const MainView = () => (
-  <div className="MainView">
-    <h1 className="MainView-title">d&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;rifter</h1>
-    <Sequencer className="MainView-panel" />
-    <Transport className="MainView-panel-center" />
-    <Visualizations className="MainView-panel-end" />
-  </div>
-)
+const MainView = () => {
+  const [isPlaying, setPlaying] = useState(false)
+  const [isRecording, setRecording] = useState(false)
+
+  return (
+    <div className="MainView">
+      <h1 className="MainView-title">d&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;rifter</h1>
+      <Sequencer
+        className="MainView-panel"
+        isRunning={isPlaying} />
+      <Transport
+        className="MainView-panel-center"
+        isPlaying={isPlaying}
+        isRecording={isRecording}
+        onPlay={() => setPlaying(true)}
+        onStop={() => { setPlaying(false); setRecording(false) }}
+        onRecord={() => setRecording(!isRecording)} />
+      <Visualizations className="MainView-panel-end" />
+    </div>
+  )
+}
 
 
 export default MainView
