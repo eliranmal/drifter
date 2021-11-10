080c56586cf1f9cec5c060009475eec6a71cd2ae
eliran <eliranmal@gmail.com>
2021-11-10 19:53:53 +0200

    chore: add hook for a tone sampler
    implementaion has the sampler synced to the transport by default

---
+++ src/hooks/useSampler.js
@@ -0,0 +1,32 @@
+import {useState} from 'react'
+import useTone from './useTone'
+
+
+const sampleMap = {
+  rolandTr808: {
+    urls: {
+      'C4': 'TR-808Kick01.wav',
+      'C#4': 'TR-808Snare01.wav',
+      'D4': 'TR-808Clap01.wav',
+      'D#4': 'TR-808Hat_C01.wav',
+    },
+    baseUrl: '/audio/drum-machines/roland-tr-808/',
+  }
+}
+
+const useSampler = (sampleMap = {}, samplerOptions = {}) => {
+  const [loaded, setLoaded] = useState(false)
+  const [error, setError] = useState(null)
+
+  return [useTone('Sampler', {
+    ...samplerOptions,
+    ...sampleMap,
+    onload: () => setLoaded(true),
+    onerror: err => setError(err),
+  }).sync(), loaded, error]
+}
+
+
+export {sampleMap}
+
+export default useSampler
