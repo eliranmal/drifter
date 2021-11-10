89d00694f75a919187b299df95882080f064397a
eliran <eliranmal@gmail.com>
2021-11-10 19:23:58 +0200

    refactor(components): add event mapping utilty to useKeyboard

--- src/hooks/useKeyboard.js
+++ src/hooks/useKeyboard.js
@@ -1,6 +1,30 @@
 import {useEffect} from 'react'
 
 
+/**
+* a functional switch statement for command-style funneling of keyboard events.
+*
+* @param {Array<Array>} mappings - a list of tupples in the form:
+*   [
+*     [ {Number} keyCode, {Array<String>} modifiers, {Function} listener ]
+*   ]
+* modifiers is optional
+*
+* @returns {Function} an event listener that is routed and ready to be attached
+*/
+const mapKeyboardEvents = mappings => e => {
+  const mapping = mappings.find(([keyCode, modifiers]) =>
+    keyCode === e.keyCode && (
+      !modifiers || modifiers.length === 0 ||
+      modifiers.every(modifier => e[modifier])
+    ))
+  if (mapping) {
+    e.preventDefault()
+    const listener = mapping[mapping.length - 1]
+    listener()
+  }
+}
+
 const useKeyboard = (listener = () => {}) => {
   useEffect(() => {
     // keydown is best for capturing backspace and such
@@ -11,5 +35,7 @@ const useKeyboard = (listener = () => {}) => {
   })
 }
 
+
+export {mapKeyboardEvents}
 
 export default useKeyboard
