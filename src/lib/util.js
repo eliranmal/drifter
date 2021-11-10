877bc40e455fb1a364fc515d3b7f6c64facb9fab
eliran <eliranmal@gmail.com>
2021-11-10 19:54:42 +0200

    refactor: add utility for matrix rotation

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,2 +1,6 @@
 
 export const range = size => [...(new Array(size)).keys()]
+
+export const rotateMatrix = matrix => range(matrix[0] && matrix[0].length)
+    .map(colIndex => matrix
+        .map((row, rowIndex) => row[colIndex]))
