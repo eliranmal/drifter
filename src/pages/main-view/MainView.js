577d7e1a21ec2343bc0d490de5b5ed807ff71d1c
eliran <eliranmal@gmail.com>
2021-11-10 20:07:23 +0200

    feat: add audio
    - hook audio engine and sampler with main view and transport
    - refactor for reuse

--- src/pages/main-view/MainView.js
+++ src/pages/main-view/MainView.js
@@ -1,6 +1,9 @@
-import {useState, useCallback} from 'react'
+import {useState, useEffect, useCallback} from 'react'
+import useLocalStorage from 'use-local-storage'
 
-import useKeyboard from '../../hooks/useKeyboard'
+import * as audio from '../../lib/audio'
+import useSampler, {sampleMap} from '../../hooks/useSampler'
+import useKeyboard, {mapKeyboardEvents} from '../../hooks/useKeyboard'
 import Sequencer from '../../components/sequencer/Sequencer'
 import Transport from '../../components/transport/Transport'
 import Visualizations from '../../components/visualizations/Visualizations'
@@ -8,33 +11,51 @@ import Visualizations from '../../components/visualizations/Visualizations'
 import './MainView.css'
 
 
+
 const MainView = () => {
   const [isPlaying, setPlaying] = useState(false)
   const [isRecording, setRecording] = useState(false)
 
-  const play = useCallback(() => !isPlaying && setPlaying(true), [isPlaying])
-  const stop = useCallback(() => { setPlaying(false); setRecording(false) }, [])
+  const [triggerMatrix, setTriggerMatrix] = useLocalStorage('drifter-trigger-matrix', [
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    // [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ])
+
+  const [sampler/*, loaded, error*/] = useSampler(sampleMap.rolandTr808)
+
+  const play = useCallback(() => {
+    if (!isPlaying) {()
+      audio.play()
+      setPlaying(true)
+    }
+  }, [isPlaying])
+
+  const stop = useCallback(() => {
+    audio.stop()
+    setPlaying(false)
+    setRecording(false)
+  }, [])
+
   const record = useCallback(() => setRecording(true), [])
+
   const togglePlay = useCallback(() => isPlaying ? stop() : play(), [isPlaying, play, stop])
 
-  useKeyboard(useCallback(e => {
-    const {keyCode, shiftKey} = e
-    switch (keyCode) {
-      case 32: // space
-        e.preventDefault()
-        togglePlay()
-        break
-      case 82: // r
-        if (shiftKey) {
-          e.preventDefault()
-          play()
-          record()
-        }
-        break
-      default:
-        break
-    }
-  }, [play, record, togglePlay]))
+
+  useEffect(() => {
+    audio.init()
+    audio.loadTriggers(sampler, triggerMatrix)
+    return () => sampler.dispose()
+  }, [sampler, triggerMatrix])
+
+  useKeyboard(useCallback(mapKeyboardEvents([
+    [32, () => togglePlay()],
+    [82, ['shiftKey'], () => {
+      play()
+      record()
+    }],
+  ]), [play, record, togglePlay]))
 
   return (
     <div className="drifter-main-view">
