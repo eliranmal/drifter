54a4eb2c560800af23b6802f3b67e8bc6be80afd
eliran <eliranmal@gmail.com>
2021-11-10 20:01:07 +0200

    feat: add library for audio related abstractions

---
+++ src/lib/audio.js
@@ -0,0 +1,48 @@
+import * as Tone from 'tone'
+import {rotateMatrix} from './util'
+
+
+// todo - how to get notes dynamically from the sampler instance?
+//        sampler.get() only seems to return the default settings
+const noteByChannelIndex = [
+  'C4',
+  'C#4',
+  'D4',
+  'D#4',
+]
+
+
+const asTransportTime = sixteenthIndex => [
+  Math.floor(sixteenthIndex / 16) % 4,
+  Math.floor(sixteenthIndex / 4) % 4,
+  sixteenthIndex % 4,
+].join(':')
+
+
+export const init = ({bpm = 99} = {}) => {
+  Tone.Transport.loop = true
+  Tone.Transport.bpm.value = bpm
+  // Tone.Transport.PPQ = 1
+}
+
+// todo - extract play() / stop() to useTransport? maybe
+export const play = () => Tone.start()
+  .then(() => Tone.Transport.start())
+
+export const stop = () => Tone.loaded()
+  .then(() => Tone.Transport.stop())
+
+export const loadTriggers = (sampler, matrix) => {
+  const firstChannel = matrix[0]
+  const sixteenthsInLoop = firstChannel && firstChannel.length
+
+  Tone.Transport.setLoopPoints(0, `${sixteenthsInLoop / 16}m`)
+
+  rotateMatrix(matrix)
+    .map(sixteenthValues => sixteenthValues
+      .map((value, channelIndex) => value && noteByChannelIndex[channelIndex])
+      .filter(Boolean))
+    .map((sixteenthNotes, sixteenthIndex) => sixteenthNotes.length &&
+        sampler.triggerAttack(sixteenthNotes, asTransportTime(sixteenthIndex))
+    )
+}
