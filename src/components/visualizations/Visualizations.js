517332e24218326f9048b949a6d18fa50db4f71b
eliran <eliranmal@gmail.com>
2021-11-10 17:12:13 +0200

    chore: add todo comment

--- src/components/visualizations/Visualizations.js
+++ src/components/visualizations/Visualizations.js
@@ -6,6 +6,7 @@ import './Visualizations.css'
 
 const Visualizations = ({className, ...props}) => (
   <div {...props} className={`drifter-visualizations ${className}`}>
+    {/* todo - put audio visualization nodes here instead of the meters */}
     <Meter className="drifter-visualization-loose" value={30} />
     <Crossfader />
     <Meter className="drifter-visualization-tight" value={80} />
