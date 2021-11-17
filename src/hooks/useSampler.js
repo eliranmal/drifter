948e880105d665bca9df00588b51c93f1080afcd
eliran <eliranmal@gmail.com>
2021-11-17 18:20:31 +0200

    refactor: sampler smart loading
    resolve sample map directory name dynamically

--- src/hooks/useSampler.js
+++ src/hooks/useSampler.js
@@ -1,27 +1,36 @@
 import {useState} from 'react'
+import kebabCase from 'lodash.kebabcase'
+
 import useToneRef from './useToneRef'
 
 
 const sampleMap = {
   rolandTr808: {
-    urls: {
-      'C4': 'TR-808Kick01.wav',
-      'C#4': 'TR-808Snare01.wav',
-      'D4': 'TR-808Clap01.wav',
-      'D#4': 'TR-808Hat_C01.wav',
-    },
-    baseUrl: './audio/drum-machines/roland-tr-808/',
-  }
+    'C4': 'TR-808Kick01.wav',
+    'C#4': 'TR-808Snare01.wav',
+    'D4': 'TR-808Clap01.wav',
+    'D#4': 'TR-808Hat_C01.wav',
+  },
 }
 
-const useSampler = (sampleMap = {}, samplerOptions = {}) => {
+const findSampleMapEntry = samplerSampleMap => Object.entries(sampleMap)
+  .find(([key, map]) => Object.is(samplerSampleMap, map))
+
+const resolveSampleMapDirName = samplerSampleMap => {
+  const sampleMapEntry = findSampleMapEntry(samplerSampleMap)
+  return sampleMapEntry && sampleMapEntry[0] && kebabCase(sampleMapEntry[0])
+}
+
+const useSampler = (samplerSampleMap = {}, samplerOptions = {}) => {
   const [loaded, setLoaded] = useState(false)
   const [error, setError] = useState(null)
 
+  const samplerKitDirName = resolveSampleMapDirName(samplerSampleMap)
+
   const analyserRef = useToneRef('Analyser')
-  const samperRef = useToneRef('Sampler', {
+  const samperRef = useToneRef('Sampler', samplerSampleMap, {
     ...samplerOptions,
-    ...sampleMap,
+    baseUrl: `audio/drum-machines/${samplerKitDirName}/`,
     onload: (...args) => {
       setLoaded(true)
       samplerOptions.onload && samplerOptions.onload()
