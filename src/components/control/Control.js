3150df67a1030f492198e11d9f423fb9b2bbd1cd
eliran <eliranmal@gmail.com>
2021-11-24 16:48:32 +0200

    feat: create control panel
    move existing content from the visualizations view

---
+++ src/components/control/Control.js
@@ -0,0 +1,33 @@
+import Meter from '../meter/Meter.js'
+import Crossfader from '../crossfader/Crossfader.js'
+
+import './Control.css'
+
+
+// todo - create a visualization based on the deviation of the tick actual delay
+//        from the intended delay (maybe a simple meter)
+
+
+// useEffect(() => {
+//   setInterval(() => {
+//     console.log(samplerAnalyser.getValue())
+//   }, 1000)
+// }, [samplerAnalyser, samplerAnalyser.getValue()])
+/*<div className="drifter-analyser-visualization"></div>*/
+
+
+const Control = ({className, onCrossfaderChange = () => {}, ...props}) => (
+  <div {...props} className={`drifter-control ${className}`}>
+    {/* todo - put audio visualization nodes here instead of the meters */}
+    <Meter className="drifter-visualization-loose" value={30} />
+    <Crossfader
+      max={100}
+      defaultValue={50}
+      onChange={value => onCrossfaderChange(value)}
+    />
+    <Meter className="drifter-visualization-tight" value={80} />
+  </div>
+)
+
+
+export default Control
