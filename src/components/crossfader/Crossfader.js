c01dec0c5684341e4bee8ca6a5f20d99cd333b69
eliran <eliranmal@gmail.com>
2021-11-24 16:47:36 +0200

    feat: debounce crossfader change listener

--- src/components/crossfader/Crossfader.js
+++ src/components/crossfader/Crossfader.js
@@ -1,14 +1,19 @@
-import Input from '../input/Input';
+import debounce from 'lodash.debounce'
+import Input from '../input/Input'
 
 import './Crossfader.css'
 
 
-const Crossfader = ({className, ...props}) => (
-  <Input
-    {...props}
-    className={`drifter-crossfader ${className}`}
-    type="range" />
-)
-
+const Crossfader = ({className, onChange, ...props}) => {
+  const debouncedOnChange = debounce(onChange, 100)
+  return (
+    <Input
+      {...props}
+      className={`drifter-crossfader ${className}`}
+      type="range"
+      onChange={({currentTarget: {value}}) => debouncedOnChange(value)}
+    />
+  )
+}
 
 export default Crossfader
