aae401488c517c3d3724b6d28739b9cf13239baa
eliran <eliranmal@gmail.com>
2021-11-24 17:00:46 +0200

    chore: remove default value for crossfader

--- src/components/control/Control.js
+++ src/components/control/Control.js
@@ -22,7 +22,6 @@ const Control = ({className, onCrossfaderChange = () => {}, ...props}) => (
     {/* <Meter className="drifter-visualization-loose" value={30} /> */}
     <Crossfader
       max={100}
-      defaultValue={50}
       onChange={value => onCrossfaderChange(value)}
     />
     {/* <Meter className="drifter-visualization-tight" value={80} /> */}
