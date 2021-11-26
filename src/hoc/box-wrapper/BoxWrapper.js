99f36a377a7359140d538d396f1b6ddb367f7439
eliran <eliranmal@gmail.com>
2021-11-26 02:40:25 +0200

    feat: allow omitting the wrapper element in box-wrapper HOC

--- src/hoc/box-wrapper/BoxWrapper.js
+++ src/hoc/box-wrapper/BoxWrapper.js
@@ -3,9 +3,15 @@ import './BoxWrapper.css'
 
 
 export const withBoxWrapper = (WrappedComponent, {
+  useWrapper = true,
   wrapperClassName = '',
-} = {}) => props => (
+} = {}) => props => useWrapper ? (
   <div className={`drifter-box-wrapper ${wrapperClassName}`}>
     <WrappedComponent {...props} />
   </div>
+) : (
+  <WrappedComponent
+    {...props}
+    className={`drifter-box-wrapper ${props.className ?? ''}`}
+  />
 )
