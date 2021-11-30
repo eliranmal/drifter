ba2867eea2d03f7434421072775bc9c08e4cddd9
eliran <eliranmal@gmail.com>
2021-12-01 01:36:19 +0200

    feat(store): add app-settings as persisted-store implementation

---
+++ src/store/app-settings.js
@@ -0,0 +1,25 @@
+import persistedStore from './persisted-store'
+
+
+  const appSettings = persistedStore(module, {
+  bpm: 99,
+  resetInterval: {
+    value: 4,
+    unit: 'bars',
+  },
+  get resetIntervalValue() {
+    return this.resetInterval.value
+  },
+  set resetIntervalValue(value) {
+    this.resetInterval.value = value
+  },
+  get resetIntervalUnit() {
+    return this.resetInterval.unit
+  },
+  set resetIntervalUnit(unit) {
+    this.resetInterval.unit = unit
+  },
+})
+
+
+export default appSettings
