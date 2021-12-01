4c31d5d59050c27207175391469555640469b288
eliran <eliranmal@gmail.com>
2021-12-02 00:24:35 +0200

    feat: update number-input to cast changed values to numbers

--- src/components/number-input/NumberInput.js
+++ src/components/number-input/NumberInput.js
@@ -4,11 +4,13 @@ import Input from '../input/Input'
 import './NumberInput.css'
 
 
-const NumberInput = ({className, ...props}) => (
+const NumberInput = ({className, onChange, ...props}) => (
   <Input
     {...props}
     className={`drifter-number-input ${className ?? ''}`}
-    type="number" />
+    type="number"
+    onChange={({currentTarget: {value}}) => onChange(+value)}
+  />
 )
 
 
